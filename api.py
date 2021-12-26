a4431216b2b595e5030c03e51fee4a4b4dc0f500
kimlolipop <nrtp.dev@gmail.com>
2021-12-26 20:05:39 +0700

    first commit

---
+++ api.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+import cv2
+import uvicorn
+import face_detection_engine
+
+import io
+from starlette.responses import StreamingResponse
+from urllib.request import urlopen
+
+app = FastAPI()
+
+
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    # print('file ', file)
+    
+    if file is not None:
+        contents = await file.read()
+        file_bytes = np.asarray(bytearray(contents), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+        
+        try:
+            image, n_faces = face_detection_engine.main(opencv_image)
+        except:
+            n_faces = face_detection_engine.main(opencv_image)
+
+        print('opencvImage ', opencv_image)
+        
+        try:
+            res, im_png = cv2.imencode(".png", image)
+            im = StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+            return im
+        except:
+            return n_face
+
+
+        
+        
+@app.post("/cap_img/")
+async def create(file: UploadFile = File(...)):
+
+    
+    if file is not None:
+
+        contents = await file.read()
+        file_bytes = np.asarray(bytearray(contents), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+
+        try:
+            image, n_faces = face_detection_engine.main(opencv_image)
+        except:
+            n_faces = face_detection_engine.main(opencv_image)
+        
+        print('opencvImage ', opencv_image)
+        
+        try:
+            res, im_png = cv2.imencode(".png", image)
+            im = StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+            return im
+        except:
+            return n_face
+
+        
+
+if __name__ == '__main__':
+    uvicorn.run('api:app',port=7001,reload=True)
---
+++ face_detection_engine.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+import sys
+import cv2
+import argparse
+
+
+
+def webcam_capture(img):
+
+    # read image data from input stream
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    return img, gray
+
+
+def face_detect(gray, cascasdepath="haarcascade_frontalface_default.xml"):
+    # process face detection
+    face_cascade = cv2.CascadeClassifier(cascasdepath)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30)
+
+    )
+    print("The number of faces found = ", len(faces))
+
+    return faces
+
+
+def render(image, faces, nogui=False):
+    # create output image
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + h, y + h), (0, 255, 0), 2)
+
+    if nogui:
+        try:
+            cv2.imwrite('detected_face.png', image)
+        except:
+            pass
+        
+        return len(faces)
+    else:
+        return image, len(faces)
+    
+
+
+def main(img):
+    parser = argparse.ArgumentParser(description='Simple Face Detection.')
+    parser.add_argument("-s", "--source",
+                        type=str,
+                        default="0",
+                        help="Source. Path to the input image.")
+
+    parser.add_argument("-m", "--model",
+                       type=str,
+                       default="haarcascade_frontalface_default.xml",
+                       help="Model path. Path to the 'haarcascade_frontalface_default.xml'.")
+
+    parser.add_argument("-u", "--nogui",
+                        type=bool,
+                        default=False,
+                        help="Enable GUI?. Disable GUI. Default False.")
+
+    args = parser.parse_args()
+    print("Using model", args.model)
+
+    # Run the face detection
+    image, gray = webcam_capture(img)      
+    detected_faces = face_detect(gray, cascasdepath=args.model)
+    
+    
+    try:
+        iamge, n_faces = render(image, detected_faces, nogui=False)
+        cv2.imshow("Faces found", image)
+        cv2.waitKey(0)
+        return image, n_faces
+
+    except:
+        n_faces = render(image, detected_faces, nogui=False)
+        return n_faces
+       
+        
+   
+
+        
